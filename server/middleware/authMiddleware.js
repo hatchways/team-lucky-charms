6f442e3650ad3512332c73c4a2197326e358351c
Anusha <anushakakaraparti@gmail.com>
2020-11-13 07:01:16 -0600

    Removed industry schema

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,17 +2,14 @@ const jwt = require("jsonwebtoken");
 const { TOKEN_SECRET_KEY } = require("../config");
 
 const requireAuth = (req, res, next) => {
-  console.log("middleware");
   const token = req.cookies.jwt;
 
   if (token) {
     jwt.verify(token, TOKEN_SECRET_KEY, (err, decodedToken) => {
       if (err) {
-        console.log("Token expired");
         // token expired
       } else {
         // user is authenticated
-        console.debug("Token exists", decodedToken);
         req.id = decodedToken.id; // get current user id
         next();
       }
