88207434e229b19d18b64e963bedf5c2dc3625c3
Anusha <anushakakaraparti@gmail.com>
2020-11-16 10:14:48 -0600

    Modified image upload API call

--- server/app.js
+++ server/app.js
@@ -1,31 +1,31 @@
-const createError = require("http-errors");
-const express = require("express");
-const { join } = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const createError = require('http-errors');
+const express = require('express');
+const { join } = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
 // config
-const { MONGODB_CON_STR } = require("./config");
+const { MONGODB_CON_STR } = require('./config');
 
 // routes
-const authRoutes = require("./routes/authRoutes");
-const projectRoutes = require("./routes/projectRoutes");
-const imageUploadRoutes = require("./routes/imageUploadRoutes");
+const authRoutes = require('./routes/authRoutes');
+const projectRoutes = require('./routes/projectRoutes');
+const imageUploadRoutes = require('./routes/imageUploadRoutes');
 
 const { json, urlencoded } = express;
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(__dirname, 'public')));
 
 app.use(authRoutes);
-app.use("/api/projects/", projectRoutes);
-app.use("/", imageUploadRoutes);
+app.use('/api/projects/', projectRoutes);
+app.use('/api/projects/', imageUploadRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -36,7 +36,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
@@ -50,10 +50,10 @@ mongoose
     autoIndex: false,
   })
   .then(() => {
-    console.log("database connected");
+    console.log('database connected');
   })
   .catch(() => {
-    console.log("There was an error connecting to the database");
+    console.log('There was an error connecting to the database');
   });
 
 module.exports = app;
