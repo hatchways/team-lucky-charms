576d2353364b45629d8ab5aa6395b3c2ba810190
Anusha <anushakakaraparti@gmail.com>
2020-11-16 04:18:03 -0600

    minor cleanup

--- server/services/aws-config.js
+++ server/services/aws-config.js
@@ -1,8 +1,7 @@
-const aws = require("aws-sdk");
+const aws = require('aws-sdk');
 const { AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID, AWS_REGION } = process.env;
 
 // To update configuration settings for aws sdk
-
 function setConfig() {
   aws.config.update({
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
