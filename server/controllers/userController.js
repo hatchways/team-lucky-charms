23994d267a429a09ee2e7c6a87489a0a175a86e9
Alex Russian <53941572+russ430@users.noreply.github.com>
2020-11-22 20:58:51 -0500

    Set password param to null on return

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,20 +5,22 @@ async function getUser(req, res) {
 
   if (!user) {
     return res.status(404);
-  };
+  }
   res.status(200).send(user);
 }
 
 async function updateUser(req, res) {
   const id = req.params.userId;
   const update = { ...req.body };
-  const user = await User.findByIdAndUpdate(id, update, { new: true });
-
-  // TODO: remove 'password' prop from returned user
+  const user = await User.findByIdAndUpdate(id, update, {
+    new: true,
+    useFindAndModify: false,
+  });
 
   if (!user) {
     return res.status(404);
   }
+  user.password = null;
   res.status(200).send(user);
 }
 
