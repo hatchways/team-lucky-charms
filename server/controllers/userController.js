0fc88725749f0c3a2ce3b07c7273873b9735b055
Alex Russian <53941572+russ430@users.noreply.github.com>
2020-11-24 11:21:16 -0500

    Added check to make sure authUser is editing own profile

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,6 +11,13 @@ async function getUser(req, res) {
 
 async function updateUser(req, res) {
   const id = req.params.userId;
+  const authId = req.id;
+
+  // authenticated user must be the same as the user being updated
+  if (id !== authId) {
+    return res.status(403);
+  }
+
   const update = { ...req.body };
   const user = await User.findByIdAndUpdate(id, update, {
     new: true,
